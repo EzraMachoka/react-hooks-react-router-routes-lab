2b606165e981a37b625baa4020b7330e4279769a
EzraMachoka <ezra.machoka@student.moringaschool.com>
2023-07-26 15:11:32 +0300

    Completed Assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,34 @@
 import React from "react";
-import { directors } from "../data";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Actors from "./Actors";
+import Directors from "./Directors";
+import Movies from "./Movies";
 
-function Directors() {
-  return <div>{/*{code here}*/}</div>;
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Switch>
+        <Route exact path="/">
+          {<Home />}
+        </Route>
+
+        <Route exact path="/movies">
+          {<Movies />}
+        </Route>
+
+        <Route exact path="/actors">
+          {<Actors />}
+        </Route>
+
+        <Route exact path="/directors">
+          {<Directors />}
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
-export default Directors;
+export default App;
